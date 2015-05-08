4f1e3a34a211f9a3f0d8d325e403bbbd17c4de2b
Ros Haitovich <ros.haitovich@kerensen.com>
2015-05-08 12:54:20 +0300

    init

---
+++ Schedule/Model/Lecturer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule.Model
+{
+    public class Lecturer
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+        }
+
+        private LinkedList<TimeConstraint> _constraints;
+        public LinkedList<TimeConstraint> Constraints
+        {
+            get { return _constraints; }
+            set { _constraints = value; }
+        }
+
+        public Lecturer(string first, string last)
+        {
+            this._firstName = first;
+            this._lastName = last;
+            Constraints = new LinkedList<TimeConstraint>();
+        }
+
+        /// <summary>
+        /// Add constraint to the list
+        /// </summary>
+        /// <param name="t"></param>
+        public void AddConstraint(TimeConstraint t)
+        {
+            foreach (var item in Constraints)
+            {
+                
+            }
+        }
+
+        /// <summary>
+        /// Get list of constrains
+        /// </summary>
+        /// <returns></returns>
+        public LinkedList<TimeConstraint> GetConstraints()
+        {
+            return Constraints;
+        }
+
+        public int DifferentDays()
+        {
+            return Constraints.Select(x => x.Day).Distinct().Count();
+        }
+
+        public int Hours()
+        {
+            int sum = 0;
+            foreach (var item in Constraints)
+                sum += item.Hours();
+            return sum;
+        }
+    }
+}
---
+++ Schedule/Model/TimeConstraint.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Schedule.Model
+{
+    public class TimeConstraint
+    {
+        private string _day;
+        public string Day
+        {
+            get { return _day; }
+            set { _day = value; }
+        }
+
+        private int _beginHour;
+        public int BeginHour
+        {
+            get { return _beginHour; }
+            set
+            {
+                if (value >= 8 && value <= 20)
+                    _beginHour = value;
+            }
+        }
+
+        private int _endHour;
+        public int EndHour
+        {
+            get { return _endHour; }
+            set
+            {
+                if (value >= 8 && value <= 20)
+                    _endHour = value;
+            }
+        }
+
+        public TimeConstraint(string day, int begin, int end)
+        {
+            this.Day = day;
+            this.BeginHour = begin;
+            this.EndHour = end;
+        }
+
+        /// <summary>
+        /// Time range
+        /// </summary>
+        public int Hours()
+        {
+            return _beginHour - _endHour;
+        }
+
+        /// <summary>
+        /// Check if constraints overlaps
+        /// </summary>
+        /// <param name="other"></param>
+        public bool Contains(TimeConstraint other)
+        {
+            // different day of week
+            if (!this.Day.Equals(other.Day))
+                return false;
+            // this start later or end befor other
+            if (this.BeginHour < other.BeginHour || this.EndHour > other.EndHour)
+                return false;
+            return true;
+        }
+
+        /// <summary>
+        /// Union 2 constraints
+        /// </summary>
+        /// <param name="other"></param>
+        public TimeConstraint Union(TimeConstraint other)
+        {
+            // different day of week
+            if (!this.Day.Equals(other.Day))
+                return null;
+            // this ends before other
+            if (this.BeginHour >= other.BeginHour && this.EndHour <= other.EndHour)
+                return null;
+            // other ends before this
+            if (other.BeginHour >= this.BeginHour && other.EndHour <= this.EndHour)
+                return null;
+            // get begin and end hours
+            int start = this.BeginHour >= other.BeginHour ? this.BeginHour : other.BeginHour;
+            int end = this.EndHour >= other.EndHour ? this.EndHour : other.EndHour;
+
+            return new TimeConstraint(this.Day, start, end);
+        }
+
+        public bool Overlaps()
+        {
+
+            return false;
+        }
+
+        /// <summary>
+        /// Constraint string representation
+        /// Example: "Sunday, from 9:00 to 12:00"
+        /// </summary>
+        public override string ToString()
+        {
+            return string.Format("{0}, from {1}:00 to {2}:00", _day, _beginHour, _endHour);
+        }
+    }
+}
