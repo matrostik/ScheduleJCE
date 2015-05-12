9c8b1c4d3ea1cceae0107e95c56cd760da15b508
Ros Haitovich <ros.haitovich@kerensen.com>
2015-05-12 15:27:21 +0300

    simple tests

--- Code/UnitTest/UnitTest.cs
+++ Code/UnitTest/UnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Schedule.Domain.Model;
 
 namespace UnitTest
 {
@@ -9,6 +10,23 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            Lecturer l = new Lecturer("Bruce", "Lee");
+            Assert.IsNotNull(l);
+            l.AddConstraint(new TimeConstraint(ConstraintDayOfWeek.Wednesday, 19, 20));
+            l.AddConstraint(new TimeConstraint(ConstraintDayOfWeek.Thursday, 8, 10));
+            l.AddConstraint(new TimeConstraint(ConstraintDayOfWeek.Monday, 8, 10));
+            Assert.AreEqual(3, l.Constraints.Count);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Lecturer l = new Lecturer("Bruce", "Lee");
+            Assert.IsNotNull(l);
+            l.AddConstraint(new TimeConstraint(ConstraintDayOfWeek.Monday, 19, 20));
+            l.AddConstraint(new TimeConstraint(ConstraintDayOfWeek.Monday, 8, 10));
+            l.AddConstraint(new TimeConstraint(ConstraintDayOfWeek.Monday, 8, 10));
+            Assert.AreEqual(2, l.Constraints.Count);
         }
     }
 }
