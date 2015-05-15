cd6f27101536c8add640b2df4577da8e00754d07
Ros Haitovich <ros.haitovich@kerensen.com>
2015-05-15 10:51:40 +0300

    Staff class

---
+++ Code/Schedule.Domain/Model/Staff.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Schedule.Domain.Model
+{
+    public class Staff
+    {
+        private LinkedList<Lecturer> _lecturers;
+        public LinkedList<Lecturer> Lecturers
+        {
+            get { return _lecturers; }
+            set { _lecturers = value; }
+        }
+
+        public Staff()
+        {
+            _lecturers = new LinkedList<Lecturer>();
+        }
+
+        /// <summary>
+        /// Get constrain for the selected lecturer
+        /// </summary>
+        /// <param name="first">First name</param>
+        /// <param name="last">Last name</param>
+        /// <returns></returns>
+        public LinkedList<TimeConstraint> GetConstraints(string first, string last)
+        {
+            var lecturer = _lecturers.FirstOrDefault(l => l.FirstName.Equals(first) && l.LastName.Equals(last));
+            return lecturer != null ? lecturer.Constraints : null;
+        }
+
+        /// <summary>
+        /// Add constraints to the lecturer's list of constraints
+        /// </summary>
+        /// <param name="first">First name</param>
+        /// <param name="second">Last name</param>
+        /// <param name="list">list of constraints</param>
+        public void AddConstraints(string first, string second, LinkedList<TimeConstraint> list)
+        {
+            var lecturer = _lecturers.FirstOrDefault(l => l.FirstName.Equals(first) && l.LastName.Equals(second));
+            if (lecturer == null)
+                return;
+            foreach (var item in list)
+            {
+                lecturer.AddConstraint(item);
+            }
+
+        }
+    }
+}
